96f3a560309c913ad63169d7a8c54c68a9872f96
Arvind Kumar <thatisarvind@gmail.com>
2023-02-22 10:28:42 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            'sans': ['Rubik','-apple-system','BlinkMacSystemFont','Arial','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif'],
+        },
+        container: {
+            center: true,
+        },
+        colors: {
+            blue: {
+                500: '#137296',
+            }
+        },
+        borderRadius: {
+            DEFAULT: '5px',
+        },
+        screens: {
+            'sm': '576px',
+            'md': '768px',
+            'lg': '992px',
+            'xl': '1200px',
+        },
+    },
   },
   plugins: [],
 }
